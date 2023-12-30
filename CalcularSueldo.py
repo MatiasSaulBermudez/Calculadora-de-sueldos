7c77156755444364f65a287840e563f852809342
Matias Saul Bermudez <130097821+MatiasSaulBermudez@users.noreply.github.com>
2023-12-29 21:36:01 -0300

    Add files via upload
    Programa realizado en forma de practica, en lenguaje python.

---
+++ CalcularSueldo.py
@@ -0,0 +1,9 @@
+# Programa para calcular el sueldo mensual de un empleado
+
+horasTrabajadas = float(input("Ingrese la cantidad de horas trabajadas:"))
+valorHora = float(input("Ingrese el valor de horas trabajadas"))
+
+# Operacion
+sueldoMensual = horasTrabajadas * valorHora
+print("El sueldo mensual por el total de horas trabajadas es de:" , sueldoMensual)
+
